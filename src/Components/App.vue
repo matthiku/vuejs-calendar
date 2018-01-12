5963bc0d4e67a5e9d33c0b17342739037111e2f2
Matthias Kuhs <matthiku@yahoo.com>
2018-01-12 13:15:24 +0000

    initial commit - creating days array

---
+++ src/Components/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div v-for="(day, index) in days" :key="index">{{ day }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      month: 1,
+      year: 2018
+    }
+  },
+  computed: {
+    days () {
+      let days = []
+      let currentDay = this.$moment(`${this.year}-${this.month}-1`, 'YYYY-M-D')
+      do {
+        days.push(currentDay)
+        currentDay = currentDay.clone().add(1, 'day')
+      } while (currentDay.month() < this.month)
+      return days
+    }
+  }
+}
+</script>
--- src/web.entry.js
+++ src/web.entry.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
+import './style.scss'
+
+import App from './Components/App.vue'
+
+import moment from 'moment-timezone'
+moment.tz.setDefault("UTC")
+// add moment to the Vue prototype, so that we can use it in all components!
+Object.defineProperty(Vue.prototype, '$moment', { get() { return this.$root.moment }})
 
 new Vue({
   el: '#app',
   data: {
-    msg: 'Hello World'
+    moment
+  },
+  components: {
+    App
   }
 });
